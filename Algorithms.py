e13f685833a4c187f555ac1f2423a78c520842bf
KVedantK <kulvedant2000@gmail.com>
2021-06-02 23:41:54 +0530

    Added Decision method to FindS

--- Algorithms.py
+++ Algorithms.py
@@ -1,24 +1,57 @@
 import csv
 class FindS_Algorithm:
-    def Find_S(file):
-        with open(file) as f:
-            reader = csv.reader(f)
-            data = list(reader)
+    def Get_Hypothesis(self,file):
+        try:
+            with open(file) as f:
+                reader = csv.reader(f)
+                data = list(reader)
 
-        hypo = ['0']*(len(data[0])-1)
-        positive_instances = []
-        for i in data:
-            if i[len(i)-1] == 'Yes':
-                positive_instances.append(i)
-        print(positive_instances)
-        for j in range(0,(len(hypo))): 
-            for i in range(0,(len(positive_instances))):
-                if hypo[j] != positive_instances[i][j] and hypo[j] == '0':
-                    hypo[j] = positive_instances[i][j]
-                elif hypo[j] != positive_instances[i][j] and hypo[j] != '0':
-                    hypo[j] = '?'
+            hypo = ['0']*(len(data[0])-1)
+            positive_instances = []
+            for i in data:
+                if i[len(i)-1].lower() == 'yes':
+                    positive_instances.append(i)
+            for j in range(0,(len(hypo))): 
+                for i in range(0,(len(positive_instances))):
+                    if hypo[j].lower() != positive_instances[i][j].lower() and hypo[j] == '0':
+                        hypo[j] = positive_instances[i][j].lower()
+                    elif hypo[j].lower() != positive_instances[i][j].lower() and hypo[j] != '0':
+                        hypo[j] = '?'
+                    else:
+                        pass
+
+            return hypo
+        except IOError:
+            print("Make Sure the File is in CSV and same directory")
+        
+        except:
+            print("Make Sure Last Column of CSV is decisive 'Yes' for positive and 'No' for negative")
+
+            
+    
+    def Get_Decision(self,hypo, query):
+        try:
+            flag = True
+            for i in range(0,len(query)):
+                if query[i].lower() == hypo[i].lower() or hypo[i] == '?':
+                    flag = True
                 else:
-                    pass
+                    flag = False
+                    break
+            if flag:
+                return True
+            else:
+                return False
+        
+        except:
+            if len(hypo) != len(query):
+                print("Hypothesis and query are not for same set of instances.")
+            
+            else:
+                print("Please Check if hypothesis is in standard form {'0' for spicific and '?' for general}")
 
-        return hypo
+
+                
+            
+
 
